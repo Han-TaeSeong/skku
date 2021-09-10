3135f467056a29726feafd7af61c03bbb00e673b
Han-TaeSeong <80900070+Han-TaeSeong@users.noreply.github.com>
2021-09-10 18:00:52 +0900

    Update Data_Preprocessing.py

--- Data_Preprocessing.py
+++ Data_Preprocessing.py
@@ -19,18 +19,21 @@ A01T = sio.loadmat('.\\BCI_Competition_4_2a\\A01E.mat')   ## 딕셔너리 class
 # print(A01T['data'])
 
 
+
 data = A01T['data']    ###  numpy.ndarray class
+
+
 
 # print(data.shape)    ##### (1,9)
 data = data.flatten()    ###(9,)
 a = data[4]
 b = list(a[0,0])
-
+#
 # print(len(b))           ###length = 8
-## b[0] length,25 EEG신호
-## b[1] events
-## b[2] target
-
+# b[0] length,25 EEG신호
+# b[1] events
+# b[2] target
+#
 eeg_raw = np.array(b[0]).transpose()  ##25 X ~~
 event = np.array(b[1]).transpose()
 target = np.array(b[2]).transpose()
@@ -39,5 +42,9 @@ eeg_info = mne.create_info(ch_names=25 ,sfreq=250, ch_types='eeg')  ##   250hz
 eeg = mne.io.RawArray(eeg_raw,info=eeg_info)
 
 
-# print(eeg)
-print(eeg.time_as_index(385))
+
+eeg.drop_channels(['22','23','24'])
+eeg.pick_channels(['1','2',3])
+eeg.plot(n_channels=10,scalings=100)
+eeg.plot_psd(fmax=30)    ### Power Spectral Density
+plt.show()
