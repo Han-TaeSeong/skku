1b3c9ad025ddfcaac32b229ce68fcfd577c3b668
Han-TaeSeong <tjd6487@naver.com>
2021-10-21 10:47:17 +0900

    ts

--- Data_Preprocessing.py
+++ Data_Preprocessing.py
@@ -135,7 +135,7 @@ def preprocessing_4_40Hz(file):
     run = run.flatten()  ## array(9)
     feature = 0  ## will (288,22,500) feature
     target = 0  ## will 288 target
-    for i in range(3, 4):
+    for i in range(3, 9):
         if file == '.\\BCI_Competition_4_2a\\A04T.mat':  ## A04T Data 결측처리
             data = run[i - 2]
         else:
@@ -156,9 +156,7 @@ def preprocessing_4_40Hz(file):
         b = mne.io.RawArray(b, info=eeg_info)
         b.rename_channels(mapping) ## 채널이름 변경
         b.set_montage('standard_1020')
-        b.filter(l_freq=8, h_freq=80)
-        b.plot(n_channels=25, scalings=30)
-        plt.show()
+        b.filter(l_freq=4, h_freq=40)
         b = b.get_data()
         for j in range(0, 48):
             beep = data[1].flatten()[j]  ##event
@@ -172,9 +170,117 @@ def preprocessing_4_40Hz(file):
 
     c = file.split('\\')[2]
     c = c.split('.')[0]
-    # np.savez(file=".\\Preprocessed_Data\\4_40Hz\\" + c + '.npz', x=feature, y=target)  ## 파일저장
+    np.savez(file=".\\Preprocessed_Data\\4_40Hz\\" + c + '.npz', x=feature, y=target)  ## 파일저장
 
-preprocessing_4_40Hz('.\\BCI_Competition_4_2a\\A04T.mat')
+
+def preprocessing_4_100Hz(file):
+    MAT = sio.loadmat(file)  ## 딕셔너리 class
+    #############  딕셔너리 참조
+    # print('Keys : ', A01T.keys())
+    # print('Values : ', A01T.values())
+    # print(A01T['__header__'])
+    # print(A01T['__version__'])
+    # print(A01T['__globals__'])
+    # print(A01T['data'])
+    #############  딕셔너리 참조
+    run = MAT['data']  ## array(1,9)
+    run = run.flatten()  ## array(9)
+    feature = 0  ## will (288,22,500) feature
+    target = 0  ## will 288 target
+    for i in range(3, 9):
+        if file == '.\\BCI_Competition_4_2a\\A04T.mat':  ## A04T Data 결측처리
+            data = run[i - 2]
+        else:
+            data = run[i]
+
+        data = data[0, 0]
+        #########################
+        # data[0] : 22eeg&3eog ///data[1] : event///data[2] : target //len(data) : 8
+        #########################
+        a = data[2].flatten()
+        if i == 3:
+            target = a.copy()
+        else:
+            target = np.concatenate((target, a))
+
+        b = data[0].transpose()
+        eeg_info = mne.create_info(ch_names=25, sfreq=250, ch_types='eeg')
+        b = mne.io.RawArray(b, info=eeg_info)
+        b.rename_channels(mapping) ## 채널이름 변경
+        b.set_montage('standard_1020')
+        b.filter(l_freq=4,h_freq=100)
+        b = b.get_data()
+        for j in range(0, 48):
+            beep = data[1].flatten()[j]  ##event
+            c = b[:22, beep + 250 * 3:beep + 250 * 5 + 125]  ## 3초 ~ 5.5초
+            # c = mne.filter.resample(c,down=250/128)  ## 250Hz --> 128Hz down_sampling (288,22,320)
+            c = c.reshape(1, 22, -1)
+            if i == 3 and j == 0:
+                feature = c.copy()
+            else:
+                feature = np.concatenate((feature, c), axis=0)
+
+    c = file.split('\\')[2]
+    c = c.split('.')[0]
+    np.savez(file=".\\Preprocessed_Data\\4_100Hz\\" + c + '.npz', x=feature, y=target)  ## 파일저장
+
+
+
+def preprocessing_4_40Hz_4s(file):
+    MAT = sio.loadmat(file)  ## 딕셔너리 class
+    #############  딕셔너리 참조
+    # print('Keys : ', A01T.keys())
+    # print('Values : ', A01T.values())
+    # print(A01T['__header__'])
+    # print(A01T['__version__'])
+    # print(A01T['__globals__'])
+    # print(A01T['data'])
+    #############  딕셔너리 참조
+    run = MAT['data']  ## array(1,9)
+    run = run.flatten()  ## array(9)
+    feature = 0  ## will (288,22,500) feature
+    target = 0  ## will 288 target
+    for i in range(3, 9):
+        if file == '.\\BCI_Competition_4_2a\\A04T.mat':  ## A04T Data 결측처리
+            data = run[i - 2]
+        else:
+            data = run[i]
+
+        data = data[0, 0]
+        #########################
+        # data[0] : 22eeg&3eog ///data[1] : event///data[2] : target //len(data) : 8
+        #########################
+        a = data[2].flatten()
+        if i == 3:
+            target = a.copy()
+        else:
+            target = np.concatenate((target, a))
+
+        b = data[0].transpose()
+        eeg_info = mne.create_info(ch_names=25, sfreq=250, ch_types='eeg')
+        b = mne.io.RawArray(b, info=eeg_info)
+        b.rename_channels(mapping) ## 채널이름 변경
+        b.set_montage('standard_1020')
+        b.filter(l_freq=4, h_freq=40)
+        b = b.get_data()
+        for j in range(0, 48):
+            beep = data[1].flatten()[j]  ##event
+            c = b[:22, beep + 250 * 4:beep + 250 * 5 + 125]  ## 3초 ~ 5.5초
+            # c = mne.filter.resample(c,down=250/128)  ## 250Hz --> 128Hz down_sampling (288,22,320)
+            c = c.reshape(1, 22, -1)
+            if i == 3 and j == 0:
+                feature = c.copy()
+            else:
+                feature = np.concatenate((feature, c), axis=0)
+
+    c = file.split('\\')[2]
+    c = c.split('.')[0]
+    np.savez(file=".\\Preprocessed_Data\\4_40Hz_4s\\" + c + '.npz', x=feature, y=target)  ## 파일저장
+
+
+
+
+
 def make_data():
     # for i in range(1, 10):
     #     i = str(i)
@@ -186,8 +292,12 @@ def make_data():
     #     preprocessing_3rd_butterworth('.\\BCI_Competition_4_2a\\A0' + i + 'E.mat')
     for i in range(1, 10):
         i = str(i)
-        preprocessing_4_40Hz('.\\BCI_Competition_4_2a\\A0' + i + 'T.mat')
-        preprocessing_4_40Hz('.\\BCI_Competition_4_2a\\A0' + i + 'E.mat')
+        preprocessing_4_100Hz('.\\BCI_Competition_4_2a\\A0' + i + 'T.mat')
+        preprocessing_4_100Hz('.\\BCI_Competition_4_2a\\A0' + i + 'E.mat')
+
+
+
+make_data()
 
 def make_channel_first():
     for k in range(1, 10):
